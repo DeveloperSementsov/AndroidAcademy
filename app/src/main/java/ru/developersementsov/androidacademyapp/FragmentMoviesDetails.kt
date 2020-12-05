0070c9232c65cdab570d1a439e3574854453efc9
Dim@n <53405415+SDprog@users.noreply.github.com>
2020-12-05 10:35:10 +0300

    homework 4

---
+++ app/src/main/java/ru/developersementsov/androidacademyapp/FragmentMoviesDetails.kt
@@ -0,0 +1,27 @@
+package ru.developersementsov.androidacademyapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesDetails : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_movies_details, container, false)
+
+        view.findViewById<TextView>(R.id.button_back)
+            .setOnClickListener {
+                activity?.supportFragmentManager?.popBackStack()
+            }
+
+        return view
+    }
+
+}
---
+++ app/src/main/java/ru/developersementsov/androidacademyapp/FragmentMoviesList.kt
@@ -0,0 +1,27 @@
+package ru.developersementsov.androidacademyapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+
+class FragmentMoviesList : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val view = inflater.inflate(R.layout.fragment_movies_list, container, false)
+
+        view.findViewById<ImageView>(R.id.movie_main_item)
+            .setOnClickListener {
+                activity?.supportFragmentManager?.beginTransaction()?.addToBackStack(null)?.replace(R.id.main_container, FragmentMoviesDetails())?.commit()
+            }
+
+        return view
+    }
+
+}
